431b1585ee2bb91a7cdfedcb5ae0f933b12fe70c
akikuno <akikuno@users.noreply.github.com>
2020-01-21 21:00:01 +0900

    rename file

--- 0_download_genome_data.sh
+++ 0_download_genome_data.sh
@@ -1,5 +1,9 @@
-mouse genome data: mm10, GRCm38
-```
+#!/bin/sh
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>
+# Download mouse genome data: mm10, GRCm38
+# >>>>>>>>>>>>>>>>>>>>>>>>>
+
 mkdir -p mouse_genome
 # Whole-genome sequence for STAR mapping
 wget -P mouse_genome ftp://ftp.ensembl.org/pub/release-99/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
@@ -7,4 +11,4 @@ wget -P mouse_genome ftp://ftp.ensembl.org/pub/release-99/fasta/mus_musculus/dna
 wget -P mouse_genome ftp://ftp.ensembl.org/pub/release-99/fasta/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz
 # Gene information
 wget -P mouse_genome ftp://ftp.ensembl.org/pub/release-99/gtf/mus_musculus/Mus_musculus.GRCm38.99.gtf.gz
-```
+
