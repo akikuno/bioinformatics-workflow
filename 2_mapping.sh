fa42958b6d2f441ce257a1045d960294a32bb3e2
Akihiro Kuno <akihiro.kuno@gmail.com>
2020-01-27 17:00:59 +0900

    STAR-RSEM for quantification

--- 2_mapping.sh
+++ 2_mapping.sh
@@ -1,18 +1,36 @@
 #!/bin/sh
 
+
+# Define threads
+# Linux and similar...
+[ -z "$threads" ] && threads=$(getconf _NPROCESSORS_ONLN 2>/dev/null | awk '{print int($0/2)}')
+# FreeBSD and similar...
+[ -z "$threads" ] && threads=$(getconf NPROCESSORS_ONLN | awk '{print int($0/2)}')
+# Solaris and similar...
+[ -z "$threads" ] && threads=$(ksh93 -c 'getconf NPROCESSORS_ONLN' | awk '{print int($0/2)}')
+# Give up...
+[ -z "$threads" ] && threads=1
+
+##----------------------------------------------
+# STAR index
+##----------------------------------------------
+
+mkdir -p mouse_index/STAR
+
+STAR \
+--runMode genomeGenerate \
+--genomeDir mouse_index \
+--genomeFastaFiles mouse_genome/Mus_musculus.GRCm38.dna.primary_assembly.fa \
+--sjdbGTFfile mouse_genome/Mus_musculus.GRCm38.99.gtf \
+--runThreadN "$threads"
+
 ##----------------------------------------------
 # STAR mapping
 ##----------------------------------------------
 
-#= parameters ==============
-star_index="PATH"
-#===========================
-
 R1=$(ls ./fastq/*R1.fq.gz)
 R2=$(ls ./fastq/*R2.fq.gz)
-
 num=$(find ./fastq/*R1.fq.gz -type f | awk '{print NR}')
-#awk '{if (NR%2!=0) a++; print a }'
 
 mkdir -p bam
 
@@ -20,10 +38,10 @@ for i in $(echo $num) ; do
     fw=$(echo $R1 | cut -d " " -f $i)
     rv=$(echo $R2 | cut -d " " -f $i)
     out_f=$(echo "$fw" |
-    sed -e "s/fastq/bam/g" -e "s/_R1.fq.gz//g")
+        sed -e "s/fastq/bam/g" -e "s/_R1.fq.gz//g")
     
     time STAR --runThreadN "$threads" \
-    --genomeDir "$star_index" \
+    --genomeDir mouse_index/STAR \
     --readFilesIn "$fw" "$rv" \
     --readFilesCommand gunzip -c \
     --outSAMtype BAM Unsorted \
@@ -42,6 +60,48 @@ for bam in ./bam/*bam ; do
 done
 
 ##----------------------------------------------
+# Quantification by RSEM
+##----------------------------------------------
+
+# Index
+mkdir -p mouse_index/RSEM/index
+rsem-prepare-reference \
+    --star \
+    --gtf mouse_genome/Mus_musculus.GRCm38.99.gtf \
+    --num-threads "$threads" \
+    mouse_genome/Mus_musculus.GRCm38.dna.primary_assembly.fa \
+    mouse_index/RSEM/index
+
+# Quantification
+fastqs="fastq fastq_trim"
+bams="bam bam_trim"
+mkdir -p ${bams}
+
+for ijk in 1 2; do
+    bam=$(echo ${bams} | cut -d " " -f ${ijk})
+    fastq=$(echo ${fastqs} | cut -d " " -f ${ijk})
+    #
+    R1=$(ls ./${fastq}/*R1.fq.gz)
+    R2=$(ls ./${fastq}/*R2.fq.gz)
+    num=$(find ./${fastq}/*R1.fq.gz -type f | awk '{print NR}')
+    #
+    for i in $(echo ${num}) ; do
+        fw=$(echo ${R1} | cut -d " " -f ${i})
+        rv=$(echo ${R2} | cut -d " " -f ${i})
+        out_f=$(echo "${fw}" |
+            sed -e "s/${fastq}/${bam}/g" -e "s/_R1.fq.gz//g")
+
+        time rsem-calculate-expression \
+            -p ${threads} \
+            --paired-end \
+            --star \
+            --output-genome-bam \
+            ${fw} ${rv} \
+            mouse_index/RSEM/index \
+            ${bam}
+    done
+done
+##----------------------------------------------
 # BigWig files to visualize by IGV
 ##----------------------------------------------
 
@@ -54,3 +114,8 @@ for bam in ./bam/*bam ; do
     --binSize 5 \
     --normalizeUsing CPM
 done
+
+##----------------------------------------------
+# Multiqc
+##----------------------------------------------
+multiqc .
