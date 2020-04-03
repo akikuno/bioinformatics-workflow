e88f4dbd1339aab7a047c82165f198fdcca500fb
akikuno <akikuno@users.noreply.github.com>
2020-04-03 15:23:48 +0900

    Update 0_download_genome_data.sh

--- 0_download_genome_data.sh
+++ 0_download_genome_data.sh
@@ -7,16 +7,20 @@
 mkdir -p mouse_genome
 
 # Whole-genome sequence for STAR mapping
-wget -P mouse_genome \
+wget -qP mouse_genome \
 ftp://ftp.ensembl.org/pub/release-99/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
-gzip -d mouse_genome/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
+gzip -d mouse_genome/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz &
 
 # cDNA for kallisto mapping
-wget -P mouse_genome \
+wget -qP mouse_genome \
 ftp://ftp.ensembl.org/pub/release-99/fasta/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz
-gzip -d mouse_genome/Mus_musculus.GRCm38.cdna.all.fa.gz
+gzip -d mouse_genome/Mus_musculus.GRCm38.cdna.all.fa.gz &
 
 # Gene information
-wget -P mouse_genome \
+wget -qP mouse_genome \
 ftp://ftp.ensembl.org/pub/release-99/gtf/mus_musculus/Mus_musculus.GRCm38.99.gtf.gz
-gzip -d mouse_genome/Mus_musculus.GRCm38.99.gtf.gz
+gzip -d mouse_genome/Mus_musculus.GRCm38.99.gtf.gz &
+
+wait
+
+exit 0
