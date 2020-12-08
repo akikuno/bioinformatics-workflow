5f34767ffb5c381a378e9e26f171dc866cc74d72
akikuno <akikuno@users.noreply.github.com>
2020-12-08 16:25:10 +0900

    update

--- scRNAseq_Smartseq2/3_qc_seurat.R
+++ scRNAseq_Smartseq2/3_qc_seurat.R
@@ -3,7 +3,7 @@
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 options(repos = "http://cran.us.r-project.org")
 if (!requireNamespace("pacman", quietly = T)) install.packages("pacman")
-pacman::p_load(tidyverse, Seurat, patchwork)
+pacman::p_load(tidyverse, Seurat, patchwork, tidyseurat)
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #! load Data
@@ -40,10 +40,11 @@ data_meta <- data_meta %>% select(-id)
 
 data <- CreateSeuratObject(counts = data_mat, meta.data = data_meta)
 data
-
+levels(data)
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #! QC
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 data_qc <- data
 # Way1: Doing it using Seurat function
 data_qc <- PercentageFeatureSet(data_qc, "^MT-", col.name = "percent_mito")
@@ -71,8 +72,27 @@ p_umap <- DimPlot(object = data_umap, reduction = "umap", group.by = "celltype",
 
 (p_umap)
 
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#! PCA
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+data_pca <- data
+
+# Run the standard workflow for visualization and clustering
+data_pca <- FindVariableFeatures(object = data_pca,
+        selection.method = "vst", nfeatures = 2000, verbose = FALSE) %>%
+    ScaleData(verbose = FALSE) %>%
+    RunPCA(npcs = 30, verbose = FALSE)
+
+p_pca <- DimPlot(object = data_pca, reduction = "pca", group.by = "celltype",
+    label = TRUE, repel = TRUE) + NoLegend()
+
+(p_pca)
+
+
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #! Save figure
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-ggsave((p_qc / p_umap), filename= "qc_umap.png")
+ggsave((p_qc) / (p_umap + p_pca), filename = "qc_umap_pca.png", width = 10, height = 7)
