4ce333423a19cd4b0b1f453f581fd8d1b39fa5a7
akikuno <akikuno@users.noreply.github.com>
2021-01-12 13:34:05 +0900

    Update release version

--- RNA-seq/0_download_genome_data.sh
+++ RNA-seq/0_download_genome_data.sh
@@ -6,20 +6,22 @@
 
 mkdir -p mouse_genome
 
-# Whole-genome sequence for STAR mapping
+release=102
+
+# Whole-genome sequence for mapping
 wget -qP mouse_genome \
-ftp://ftp.ensembl.org/pub/release-99/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz &&
+ftp://ftp.ensembl.org/pub/release-"$release"/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz &&
 gzip -d mouse_genome/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz &
 
 # cDNA for kallisto mapping
 wget -qP mouse_genome \
-ftp://ftp.ensembl.org/pub/release-99/fasta/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz &&
+ftp://ftp.ensembl.org/pub/release-"$release"/fasta/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz &&
 gzip -d mouse_genome/Mus_musculus.GRCm38.cdna.all.fa.gz &
 
 # Gene information
 wget -qP mouse_genome \
-ftp://ftp.ensembl.org/pub/release-99/gtf/mus_musculus/Mus_musculus.GRCm38.99.gtf.gz &&
-gzip -d mouse_genome/Mus_musculus.GRCm38.99.gtf.gz &
+ftp://ftp.ensembl.org/pub/release-"$release"/gtf/mus_musculus/Mus_musculus.GRCm38."$release".gtf.gz &&
+gzip -d mouse_genome/Mus_musculus.GRCm38."$release".gtf.gz &
 
 wait
 
