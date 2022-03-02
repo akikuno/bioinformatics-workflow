678cc25885581033e6f4e91cf3cd1a24096fb0bf
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-02 16:41:43 +0900

    Update 0_download_genome_data.sh

--- RNA-seq/0_download_genome_data.sh
+++ RNA-seq/0_download_genome_data.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# Download mouse genome data: mm10, GRCm38
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+###############################################################################
+# Download mouse genome data: GRCm38 (Ensembl release 102)
+###############################################################################
 
 mkdir -p mouse_genome
 
@@ -10,18 +10,18 @@ release=102
 
 # Whole-genome sequence for mapping
 wget -qP mouse_genome \
-ftp://ftp.ensembl.org/pub/release-"$release"/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz &&
-gzip -d mouse_genome/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz &
+    ftp://ftp.ensembl.org/pub/release-"$release"/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz &&
+    gzip -d mouse_genome/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz &
 
 # cDNA for kallisto mapping
 wget -qP mouse_genome \
-ftp://ftp.ensembl.org/pub/release-"$release"/fasta/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz &&
-gzip -d mouse_genome/Mus_musculus.GRCm38.cdna.all.fa.gz &
+    ftp://ftp.ensembl.org/pub/release-"$release"/fasta/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz &&
+    gzip -d mouse_genome/Mus_musculus.GRCm38.cdna.all.fa.gz &
 
 # Gene information
 wget -qP mouse_genome \
-ftp://ftp.ensembl.org/pub/release-"$release"/gtf/mus_musculus/Mus_musculus.GRCm38."$release".gtf.gz &&
-gzip -d mouse_genome/Mus_musculus.GRCm38."$release".gtf.gz &
+    ftp://ftp.ensembl.org/pub/release-"$release"/gtf/mus_musculus/Mus_musculus.GRCm38."$release".gtf.gz &&
+    gzip -d mouse_genome/Mus_musculus.GRCm38."$release".gtf.gz &
 
 wait
 
