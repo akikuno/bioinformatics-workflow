7e2464a8e423b58c888d460e6b34240be90cf60c
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-29 15:22:12 +0900

    Update 3_featurecount.sh

--- RNA-seq/3_featurecount.sh
+++ RNA-seq/3_featurecount.sh
@@ -12,3 +12,7 @@ featureCounts -t exon -g gene_name -a "$gtf" \
   -o reports/count_gene_name.txt data/bam/*.bam
 
 gzip -c reports/raw_gene_counts_matrix.txt >reports/raw_gene_counts_matrix.txt.gz
+
+## MD5
+
+md5sum reports/raw_gene_counts_matrix.txt.gz >reports/md5_counts.txt
