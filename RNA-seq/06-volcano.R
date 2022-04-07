c23af67ff154aae68aac718402aff9b0a41856a5
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-04-07 15:26:25 +0900

    update

--- RNA-seq/06-volcano.R
+++ RNA-seq/06-volcano.R
@@ -18,6 +18,9 @@ file <- "reports/degs.csv"
 
 tcc_result <- read_csv(file)
 
+annotation <- c("ko-low", "ko-high")
+legends <- c("KO DOWN", "KO UP")
+
 ###############################################################################
 # Volucano and MA plots
 ###############################################################################
@@ -26,21 +29,23 @@ qval_min10 <- sort(tcc_result$q.value)[10]
 mval_min10 <- sort(tcc_result$m.value)[10]
 mval_max10 <- sort(tcc_result$m.value, decreasing = TRUE)[10]
 
-tcc_result <- tcc_result %>%
+tcc_volcano <-
+    tcc_result %>%
     mutate(log10qval = -log10(q.value)) %>%
     mutate(label = case_when(
-        estimatedDEG == 1 & high_exp == "mcto" ~ "MCTO-UP",
-        estimatedDEG == 1 & high_exp == "wt" ~ "MCTO-DOWN",
+        estimatedDEG == 1 & expression == annotation[1] ~ legends[1],
+        estimatedDEG == 1 & expression == annotation[2] ~ legends[2],
         TRUE ~ "NO"
     )) %>%
     mutate(annotate = case_when(
-        estimatedDEG == 1 & (q.value <= qval_min10 | m.value <= mval_min10 | m.value >= mval_max10) ~ gene_id,
+        estimatedDEG == 1 & str_detect(gene_id, TARGETS) ~ gene_id,
         TRUE ~ as.character(NA)
     ))
 
 
 mycolors <- c("#5588BB", "#F06060", "#E5E5E5")
-names(mycolors) <- c("MCTO-DOWN", "MCTO-UP", "NO")
+names(mycolors) <- c(legends[1], legends[2], "NO")
+
 
 g_volcano <-
     ggplot(tcc_result, aes(x = m.value, y = log10qval, color = label, label = annotate)) +
