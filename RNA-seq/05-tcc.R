d97c2f5c88fd66c4432e6c119a8416c648f8ba44
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-04-13 16:26:42 +0900

    Update 05-tcc.R

--- RNA-seq/05-tcc.R
+++ RNA-seq/05-tcc.R
@@ -3,7 +3,7 @@
 ###############################################################################
 
 options(repos = "http://cran.us.r-project.org")
-if (!requireNamespace("pacman", quietly = T)) install.packages("pacman")
+if (!require("pacman", quietly = TRUE)) install.packages("pacman")
 pacman::p_load(tidyverse, writexl, BiocManager, TCC)
 
 ###############################################################################
@@ -11,16 +11,26 @@ pacman::p_load(tidyverse, writexl, BiocManager, TCC)
 ###############################################################################
 
 file <- "reports/raw_gene_counts_matrix.txt.gz"
-sample_name <- c("wt-1", "wt-2", "wt-3", "mcto-1", "mcto-2", "mcto-3")
-group <- c(1, 1, 1, 2, 2, 2)
+samples <- c("cko", "ff")
+target <- "cko"
+nums <- c(4, 4)
 qval <- 0.05
+
+
+sample_name <- vector()
+group <- vector()
+for (i in seq_along(nums)) {
+    sample_name <- append(sample_name, map2_chr(samples[i], seq(nums[i]), ~ str_c(.x, .y, sep = "-")))
+    group <- append(group, rep(i, nums[i]))
+}
+
 
 ###############################################################################
 # Import data
 ###############################################################################
 
 data <- read_tsv(file, skip = 1) %>% mutate(gene_id = toupper(Geneid))
-colsubs <- seq(sample_name) + 6
+colsubs <- 7:(sum(nums) + 6)
 colnames(data)[colsubs] <- sample_name
 
 data <- data[rowSums(data[colsubs]) > 0, ]
@@ -33,28 +43,31 @@ data_tcc <- data[, colsubs] %>% as.matrix()
 rownames(data_tcc) <- toupper(data$Geneid)
 
 tcc <- new("TCC", data_tcc, group) %>%
-    filterLowCountGenes() %>%
     calcNormFactors(norm.method = "tmm", test.method = "edger", iteration = 3) %>%
     estimateDE(test.method = "edger", FDR = qval)
 
-tmp_df <- data %>%
+compare_expression <- data %>%
     select(gene_id, sample_name) %>%
     pivot_longer(-gene_id, names_to = "sample", values_to = "value") %>%
-    mutate(group = str_remove(sample, "-[123]")) %>%
-    group_by(gene_id, group) %>%
+    mutate(genotype = str_remove(sample, "-[0-9]+")) %>%
+    group_by(gene_id, genotype) %>%
     mutate(mean = mean(value)) %>%
-    select(gene_id, group, mean) %>%
-    ungroup(group) %>%
+    select(gene_id, genotype, mean) %>%
+    ungroup(genotype) %>%
+    distinct() %>%
     slice_max(mean, n = 1) %>%
-    mutate(high_exp = group) %>%
+    mutate(expression = case_when(
+        genotype == target ~ str_c(target, "-high"),
+        genotype != target ~ str_c(target, "-low")
+    )) %>%
     ungroup() %>%
-    select(gene_id, high_exp) %>%
+    select(gene_id, expression) %>%
     distinct()
 
 tcc_result <- getResult(tcc, sort = TRUE) %>%
     as_tibble() %>%
     mutate(estimatedDEG = if_else(q.value < qval, 1, 0)) %>%
-    inner_join(tmp_df) %>%
+    inner_join(compare_expression) %>%
     group_by(gene_id) %>%
     arrange(desc(estimatedDEG), q.value) %>%
     select(-rank) %>%
@@ -62,15 +75,22 @@ tcc_result <- getResult(tcc, sort = TRUE) %>%
 
 sum(tcc_result$estimatedDEG == 1)
 
-tcc_deg_mcto <- tcc_result %>% filter(estimatedDEG == 1, high_exp == "mcto")
-tcc_deg_wt <- tcc_result %>% filter(estimatedDEG == 1, high_exp == "wt")
-
 ###############################################################################
 # Export results
 ###############################################################################
 
+tcc_list <- tcc_result %>%
+    filter(estimatedDEG == 1) %>%
+    group_split(expression) %>%
+    as.list()
+
+for (i in seq_along(tcc_list)) {
+    names(tcc_list)[i] <- tcc_list[[i]]$expression[1]
+}
+tcc_list <- append(list(all = tcc_result), tcc_list)
+
 write_xlsx(
-    list(all = tcc_result, mcto_up = tcc_deg_mcto, wt_up = tcc_deg_wt),
+    tcc_list,
     "reports/degs.xlsx"
 )
 
